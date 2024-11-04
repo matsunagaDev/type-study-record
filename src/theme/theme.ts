373ff50fc44d9233e739d8b3d406d09096f1a55c
masarumatsunaga <65140122+masarumatsunaga@users.noreply.github.com>
2024-11-04 11:02:23 +0900

    update chakra-ui v3

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,12 @@
-import { extendTheme } from '@chakra-ui/react';
+import { createSystem, defaultConfig } from '@chakra-ui/react';
 
-export const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        backgroundColor: 'gray.100',
-        color: 'gray.800',
+export const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        body: {
+          value: 'sans-serif',
+        },
       },
     },
   },
